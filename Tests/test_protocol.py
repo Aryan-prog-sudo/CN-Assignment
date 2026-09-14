df11b3745abc146bdb0ec703e8e07663853bd0ef
Aryan-prog-sudo <aryanrana82105181@gmail.com>
2026-09-14 16:36:08 +0530

    Added the report and the protocol

--- Tests/test_protocol.py
+++
@@ -1,9 +0,0 @@
-from Common.protocol import *
-import socket
-
-server_socket, client_socket = socket.socketpair()
-SendCommand(client_socket, "Login Alice")
-command = ReceiveCommand(server_socket)
-print("Received command:", command)
-server_socket.close()
-client_socket.close()
